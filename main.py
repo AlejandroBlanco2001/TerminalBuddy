e58109957cd3ee5109e66d5e8e789cc2375c2688
Isaac Blanco <nuclearhands2001@gmail.com>
2026-03-09 17:18:17 -0500

    add: creating the fast api server

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from terminal-friend!")
+from google.adk.cli.fast_api import get_fast_api_app
+import uvicorn
+from pathlib import Path
 
+ROOT_FOLDER = Path(__file__).parent
+AGENTS_DIR = ROOT_FOLDER
+
+app = get_fast_api_app(
+    agents_dir=AGENTS_DIR,
+    web=False,
+)
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
