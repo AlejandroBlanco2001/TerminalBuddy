892327980471ce74c6ee5110100774de8f148033
Isaac Blanco <nuclearhands2001@gmail.com>
2026-03-09 17:32:31 -0500

    add: docker compose and short term memory

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from google.adk.cli.fast_api import get_fast_api_app
+from google.adk.sessions.database_session_service import DatabaseSessionService
 import uvicorn
 from pathlib import Path
 
 ROOT_FOLDER = Path(__file__).parent
 AGENTS_DIR = ROOT_FOLDER
 
+session_service = DatabaseSessionService(
+    db_url="sqlite:///./session.db"
+)
+
 app = get_fast_api_app(
     agents_dir=AGENTS_DIR,
     web=False,
+    session_service=session_service,
 )
 
 if __name__ == "__main__":
