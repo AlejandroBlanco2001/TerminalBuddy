7f05ea69f42097bd6ca61cecbcc7dffaefcda40f
Isaac Blanco <nuclearhands2001@gmail.com>
2026-03-12 15:49:11 -0500

    add: tools to the agent

--- terminal/agent.py
+++ terminal/agent.py
@@ -1,16 +1,18 @@
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from .prompts import GENERAL_INSTRUCTIONS
+from .tools import run_command, read_output
 
 model = LiteLlm(
     model='openai/gpt-4o',
-) 
+)
 
 root_agent = LlmAgent(
     model=model,
     name='root_agent',
-    description='A helpful assistant for terminal command outputs.',
+    description='A senior software engineer specialized in debugging and troubleshooting software projects through terminal-based investigation.',
     static_instruction=GENERAL_INSTRUCTIONS,
     instruction="Default: Be concise and to the point.",
+    tools=[run_command, read_output],
 )
 
