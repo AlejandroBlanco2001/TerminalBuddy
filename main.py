c3875cb12a40854b00505a4047595d039e109d2b
Isaac Blanco <nuclearhands2001@gmail.com>
2026-03-12 19:50:27 -0500

    add: HITL to avoid risky commands

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ AGENTS_DIR = ROOT_FOLDER
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     terminal_module.session = TerminalSession()
+    terminal_module.session.send("uv run example/main.py")
     yield
     terminal_module.session.terminate()
     terminal_module.session = None
--- terminal/agent.py
+++ terminal/agent.py
@@ -1,19 +1,32 @@
 from google.adk.agents.llm_agent import LlmAgent
+from google.adk.apps.app import App
+from google.adk.apps.app import ResumabilityConfig
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.function_tool import FunctionTool
+from .mcp import context7_mcp, filesystem_mcp
 from .prompts import GENERAL_INSTRUCTIONS
 from .tools import run_command, read_output
-from .mcp import context7_mcp, filesystem_mcp
 
 model = LiteLlm(
-    model='openai/gpt-4o',
+    model="openai/gpt-4o",
 )
 
 root_agent = LlmAgent(
     model=model,
-    name='root_agent',
-    description='A senior software engineer specialized in debugging and troubleshooting software projects through terminal-based investigation.',
+    name="root_agent",
+    description="A senior software engineer specialized in debugging and troubleshooting software projects through terminal-based investigation.",
     static_instruction=GENERAL_INSTRUCTIONS,
     instruction="Default: Be concise and to the point.",
-    tools=[run_command, read_output, context7_mcp, filesystem_mcp],
+    tools=[
+        FunctionTool(run_command, require_confirmation=True),
+        read_output,
+        context7_mcp,
+        filesystem_mcp,
+    ],
 )
 
+app = App(
+    name="terminal",
+    root_agent=root_agent,
+    resumability_config=ResumabilityConfig(is_resumable=True),
+)
